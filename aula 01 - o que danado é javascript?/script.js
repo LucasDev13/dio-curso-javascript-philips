9d6cc3eb623fe7afca3827214058c905646d6df9
LucasDev13 <lucaspontes140@gmail.com>
2022-05-16 19:34:45 -0300

    incluido as aulas 01 e 02.

---
+++ aula 01 - o que danado é javascript?/script.js
@@ -0,0 +1,1 @@
+alert("Olá java script!")
---
+++ aula 02 - entendendo variáveis e seus valores/script.js
@@ -0,0 +1,27 @@
+//tipos primitivos 
+
+//boolean
+/**var vOuF = false;
+console.log(typeof(vOuF));
+
+//number
+var numeroQualquer = 2;
+console.log(typeof(numeroQualquer));
+
+//string
+var nome = 'diana';
+console.log(typeof(nome));*/
+
+//Declaração de variáveis
+
+var variavel = 'lucas';
+variavel = 'fernando';//pode mudar
+console.log(variavel);
+
+let variavel2 = 'lucas';
+variavel2 = 'fernando';//pode mudar
+console.log(variavel2);
+
+const constante = 'lucas';
+//constante = 'fernando';//não pode mudar
+console.log(constante);
