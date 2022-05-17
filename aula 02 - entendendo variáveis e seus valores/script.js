4f51d2e92de97130d2e07120b49e1b232379eb5b
LucasDev13 <lucaspontes140@gmail.com>
2022-05-16 21:53:58 -0300

    trabalhando com escopo de variáveis, operadores matemáticos, lógicos e relacionais.

--- aula 02 - entendendo variáveis e seus valores/script.js
+++ aula 02 - entendendo variáveis e seus valores/script.js
@@ -14,14 +14,100 @@ console.log(typeof(nome));*/
 
 //Declaração de variáveis
 
-var variavel = 'lucas';
+//var = escopo global e local
+/*var variavel = 'lucas';
 variavel = 'fernando';//pode mudar
-console.log(variavel);
+console.log(variavel);*/
 
-let variavel2 = 'lucas';
+//let = por convenção usada em escopo local de bloco, pode ter seu valor alterado
+/*let variavel2 = 'lucas';
 variavel2 = 'fernando';//pode mudar
-console.log(variavel2);
+console.log(variavel2);*/
 
-const constante = 'lucas';
+//const = escopo local, somente leitura com valore fixo, não pode ser alterado
+/*const constante = 'lucas';
 //constante = 'fernando';//não pode mudar
-console.log(constante);
+console.log(constante);*/
+
+//está sendo vista por todo o código
+/*var escopoGlobal = 'global';
+console.log(escopoGlobal);*/
+
+/*function escopoLocal(){
+    //só posso usar dentro do escopo interdo do bloco de código
+    //ele foi definido mas não está sendo visto externamento ao bloco de código
+    let escopoLocalInterno = 'local';
+    console.log(escopoLocalInterno);
+}*/
+
+//vai chamar a função e expor os dados vindos da função.
+//escopoLocal();
+
+//atribuição ( = ) atribui um valor em uma variável
+/*var atribuicao = 'diana';
+console.log(atribuicao);
+
+//comparação ( == ) compara os valores
+var comparacao = '0' == 0;
+console.log(comparacao);
+
+//comparação identica ( === ) compara os valores e o 'tipos' 
+var comparacaoIdentica = '0' === 0;
+console.log(comparacaoIdentica);*/
+
+
+//Operadores aritméticos 
+//adicao
+/*var adicao = 1 + 1;
+console.log('adicao 1 + 1 = ' + adicao);
+
+//subtração
+var subtracao = 2 - 1;
+console.log('subtração 2 - 1 = ' + subtracao);
+
+//multiplicacao
+var multiplicacao = 2 * 3;
+console.log('multiplicacao 2 * 3 = ' + multiplicacao);
+
+//divicao real
+var divisaoReal = 6 / 2;
+console.log('divicao real 6 / 2 = ' + divisaoReal);
+
+//divisao inteira
+var divisaoInteira = 5 % 2;
+console.log('divisao inteira 5 % 2 = ' + divisaoInteira);
+
+//potenciação
+var potenciacao = 2 ** 10;
+console.log('potenciacao 2 ** 10 = ' + potenciacao);*/
+
+//Operadores relacionais
+//maior que
+/*var maiorQue = 5 > 2;
+console.log(maiorQue);
+
+//menor que
+var menorQue = 5 < 2;
+console.log(menorQue);
+
+//maior ou igual a
+var maiorOuIgual = 5 >= 2;
+console.log(maiorOuIgual);
+
+//menor ou igual a
+var menorOuIgual = 5 <= 2;
+console.log(menorOuIgual);*/
+
+
+//Operadores lógicos
+//operador &&
+/*var e = true && true;
+console.log(e);
+
+//operador ||
+var ou = false || true;
+console.log(e);
+
+//operador !
+var nao = !true;
+console.log(nao);*/
