cbd66adc40e4461b3f6571702e47c3b146978377
LucasDev13 <lucaspontes140@gmail.com>
2022-05-19 00:27:32 -0300

    Separando os if e usando switch case

--- aula 04 - estruturas condicionais/script.js
+++ aula 04 - estruturas condicionais/script.js
@@ -1,17 +1,35 @@
-var jogador1 = 1;
-var jogador2 = 0;
+var jogador1 = 0;
+var jogador2 = 1;
 var placar;
 
 //if ternário
 jogador1 != -1 && jogador2 != -1 ? console.log('Os jogadores são válidos'):
 console.log('Jogadores inválidos')
 
-if(jogador1 != -1){
-if (jogador1 > 0) {
+//usando if
+if (jogador1 > 0 && jogador2 == 0) {
     console.log('Jogador 1 marcou ponto!');
-}else if(jogador2 > 0){
+    placar = jogador1 > jogador2;
+    console.log(placar);
+}
+//usando else if
+else if(jogador2 > 0 && jogador1 == 0){
     console.log('Jogador 2 marcou ponto!');
-}else{
+    placar = jogador2 > jogador1;
+    console.log(placar);
+}
+//usando else
+else{
     console.log('Ninguem marcou pontos!')
 }
+
+switch(placar){
+    case placar = jogador1 > jogador2:
+        console.log('jogador 1 ganhou!');
+        break;
+    case placar = jogador2 > jogador1:
+        console.log('Jogador 2 ganhou!');
+        break;
+    default:
+        console.log('ninguem ganhou!')
 }
