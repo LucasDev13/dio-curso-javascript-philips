fb7809213e110e4fb70948b3976fb0ddc3dc7bd8
LucasDev13 <lucaspontes140@gmail.com>
2022-05-18 21:41:57 -0300

    Trabalhando com arrays

--- aula 03 - Array e Object/script.js
+++ aula 03 - Array e Object/script.js
@@ -4,11 +4,33 @@
 /*let array = ['string', 1, true];
 console.log(array);*/
 
-//pode guardar vários tipos de dados
+//pode guardar vários tipos de dados de diferentes tipos
 let array1 = ['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
-console.log(array1);
+//console.log(array1);
 
 //pegando um único indice
-let array2 = ['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
-console.log('array 2 -> ' + array2[0]);//pega o indice zero.
+/*let array2 = ['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
+console.log('array 2 -> ' + array2[0]);//pega o indice zero.*/
+
+/*array1.forEach(function(item, index){console.log(item, index)});*/
+
+/*array1.push('novo item');
+console.log(array1);*/
+
+/*array1.pop();
+console.log(array1);*/
+
+/*array1.shift();
+console.log(array1);*/
+
+/*array1.unshift('novo item no inicio');
+console.log(array1);*/
+
+//console.log(array1.indexOf(true));
+
+/*array1.splice(0, 3);
+console.log(array1);*/
+
+/*let novoArray = array1.slice(0, 3);
+console.log(novoArray);*/
 
