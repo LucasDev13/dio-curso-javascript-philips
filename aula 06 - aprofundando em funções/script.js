4aad7372c92c537c689eed1cc99d0f3ad6251b07
LucasDev13 <lucaspontes140@gmail.com>
2022-05-19 22:08:29 -0300

    Aprofundando em funções.

---
+++ aula 06 - aprofundando em funções/script.js
@@ -0,0 +1,28 @@
+//Tipos de funções
+
+//Declarativas - ele precisa ter um nome obrigatoriamente, sem nome ela para de funcionar
+// function funcao(){
+//     console.log('Tudo certo na fucao declarativa')
+// }
+
+// funcao();
+
+//expressão de funções
+//com nomeação
+// var funcao = function nomeFuncao(){
+//     console.log('Sou uma mensagem de função de expressão com nomeação.')
+// }
+// funcao();
+
+// //sem nomeação. Vai ser atribuido o nome da variável ao nome da função
+// var funcao = function(){
+//     console.log('Sou uma mensagem de função de expressão sem nomeação.')
+// }
+// funcao();
+
+//arrow function
+// var funcao = () => {
+//     console.log('Sou uma arrow function.')
+// }
+
+// funcao();
