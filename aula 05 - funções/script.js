71bea834506a7b02d9a4f4051dbab45d43606832
LucasDev13 <lucaspontes140@gmail.com>
2022-05-19 21:52:30 -0300

    Trabalhando com funções

---
+++ aula 05 - funções/script.js
@@ -0,0 +1,16 @@
+//declaração de uma função
+//() - indica que é um objeto do tipo function
+//{} - indica que é um bloco de instrução
+// function funcao(){
+//     //bloco de instruções
+//     console.log('Tudo certo!')
+// }
+
+// //chamando a função
+// funcao()
+
+function mensagem(primeiro, segundo){
+    console.log(primeiro, segundo)
+}
+
+mensagem('tudo certo', 'na função')
